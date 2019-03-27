cb00a5c275de4518f0cb54f307ca50c312efbf7a
Art G <theartofnoise@gmail.com>
2019-03-27 15:41:06 -0400

    added state input

--- public/js/index.js
+++ public/js/index.js
@@ -43,24 +43,20 @@ var API = {
 var refreshExamples = function() {
   API.getExamples().then(function(data) {
     var $examples = data.map(function(example) {
-      var $a = $("<a>")
-        .text(example.text)
-        .attr("href", "/example/" + example.id);
-
-      var $li = $("<li>")
-        .attr({
-          class: "list-group-item",
-          "data-id": example.id
-        })
-        .append($a);
-
-      var $button = $("<button>")
-        .addClass("btn btn-danger float-right delete")
-        .text("ｘ");
-
-      $li.append($button);
-
-      return $li;
+      // var $a = $("<a>")
+      //   .text(example.text)
+      //   .attr("href", "/example/" + example.id);
+      // var $li = $("<li>")
+      //   .attr({
+      //     class: "list-group-item",
+      //     "data-id": example.id
+      //   })
+      //   .append($a);
+      // var $button = $("<button>")
+      //   .addClass("btn btn-danger float-right delete")
+      //   .text("ｘ");
+      // $li.append($button);
+      // return $li;
     });
 
     $exampleList.empty();
